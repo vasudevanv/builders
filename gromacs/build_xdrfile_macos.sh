9353d50beb70bcef1754813921cf750669a7fa4d
Vasudevan Venkateshwaran <vasu.chemical@gmail.com>
2017-02-20 09:21:43 -0500

    Fancy argument parsing
    Added code to parse command line arguments to get the source tar
    archive and the version number. This allows for storing tars
    anywhere instead of the current directory.

--- gromacs/build_xdrfile_macos.sh
+++ gromacs/build_xdrfile_macos.sh
@@ -13,16 +13,58 @@
 # The script assumes that mpi is available as a module
 # which can be loaded using module load mpi/openmpi-x86_64
 #
-# This script was tested on Scientific Linux 7 with xdrfile-1.1.4
+# This script was tested on macOS Sierra with xdrfile-1.1.4
+
+# Argument parsing
+die()
+{
+	local _ret=$2
+	test -n "$_ret" || _ret=1
+	test "$_PRINT_HELP" = yes && print_help >&2
+	echo "$1" >&2
+	exit ${_ret}
+}
+
+print_help ()
+{
+	echo "Build script for gromacs xdrfile"
+	printf 'Usage: %s [-h|--help] <src_archive> <version>\n' "$0"
+	printf "\t%s\n" "<src_archive>: source tar file"
+	printf "\t%s\n" "<version>: version"
+	printf "\t%s\n" "-h,--help: Prints help"
+}
+
+while test $# -gt 0
+do
+    _key="$1"
+    case "$_key" in
+	-h|--help)
+	    print_help
+	    exit 0
+	    ;;
+	*)
+	    _positionals+=("$1")
+	    ;;
+    esac
+    shift
+done
+
+_positional_names=('arg_src_archive' 'arg_version')
+test ${#_positionals[@]} -lt 2 && _PRINT_HELP=yes die "FATAL ERROR: Not enough arguments - require 2, but got only ${#_positionals[@]}." 1
+test ${#_positionals[@]} -gt 2 && _PRINT_HELP=yes die "FATAL ERROR: Spurious arguments - require 2, but got ${#_positionals[@]}." 1
+for (( ii = 0; ii < ${#_positionals[@]}; ii++))
+do
+    eval "${_positional_names[ii]}=\${_positionals[ii]}" || die "Error during argument parsing, possibly a bug in the script." 1
+done
 
 # Set working directory
 working_dir=`pwd`
  
 # Gromacs source archive directory
-src_dir=`pwd`
+src_archive=${arg_src_archive}
 
 # Version
-xdr_version="1.1.4"
+xdr_version=${arg_version}
  
 # Installation directory
 install_dir=${HOME}/Applications/xdrfile-${xdr_version}
@@ -37,7 +79,7 @@ if [ -d ${temp_dir}/xdrfile-${xdr_version} ]
 then
     rm -rf ${temp_dir}/xdrfile-${xdr_version}
 fi
-tar -xzf ${src_dir}/xdrfile-${xdr_version}.tar.gz
+tar -xzf ${src_archive}
 cd ${temp_dir}/xdrfile-${xdr_version}
  
 # Build xdrfile
